093103773fcaed7b6a178adf3287a7a1a6bfefaf
stvkre <kiariemburuh@gmail.com>
2017-04-28 16:21:55 +0300

    HeroTest.java
    test to show that the user can crete an instance of the Hero class

---
+++ src/test/HeroTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+// test to show if user can create an instance of the Class Hero
+
+public class HeroTest {
+
+  public Hero(String description) {
+
+    @Test
+    public void Hero_instantiatesCorrectly_true() {
+      Hero myHero = new Hero("engage");
+      assertEquals(true, myHero instanceof Hero);
+    }
+  }
+
+}
