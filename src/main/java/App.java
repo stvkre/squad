1019207594c9ea931ed5c02ce56f0912afa9a2ad
stvkre <kiariemburuh@gmail.com>
2017-05-04 12:02:46 +0300

    App.java
    Creted route to heroes.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,17 +19,23 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+    // fetching data from the layout page
+
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    // retrieving data from the success page
+
     get("/success", (request, response) -> {
      Map<String, Object> model = new HashMap<String, Object>();
      model.put("template", "templates/success.vtl");
      return new ModelAndView(model, layout);
    }, new VelocityTemplateEngine());
+
+   // displaying data from the success page
 
   post("/success", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
@@ -42,26 +48,15 @@ public class App {
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
-// post("/success", (request, response) -> {
-//     Map<String, Object> model = new HashMap<String, Object>();
-//
-//     ArrayList<Hero> heroes = request.session().attribute("heroes");
-//     if (heroes == null) {
-//       heroes = new ArrayList<Hero>();
-//       request.session().attribute("heroes", heroes);
-//     }
-//
-//     String name = request.queryParams("name");
-//     int age = Integer.parseInt(request.queryParams("age"));
-//     String power = request.queryParams("power");
-//     String weakness = request.queryParams("weakness");
-//     Hero newHero = new Hero();
-//     heroes.add(newHero);
-//
-//     model.put("template", "templates/success.vtl");
-//     return new ModelAndView(model, layout);
-//    }, new VelocityTemplateEngine());
+// retrieving data from the heroes page
 
+get("/heroes", (request, response) -> {
+ Map<String, Object> model = new HashMap<String, Object>();
+ model.put("template", "templates/heroes.vtl");
+ return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
+// displaying data from the heroes page
    post("/heroes", (request, response) -> {
        Map<String, Object> model = new HashMap<String, Object>();
 
